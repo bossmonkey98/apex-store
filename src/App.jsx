2d56e26f9e5065cf9e6f4f745ccc8c615fcb3ae0
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-03-25 11:17:01 +0530

    Update: Productlisting page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css';
-import Slider from './components/Slider';
-import Newsletter from './components/Newsletter/Newsletter'
-import { Footer } from './components/Footer/Footer';
+import {Footer} from './components/Footer/Footer'
+import {Navbar} from './components/Navbar/Navbar'
+import Home from './pages/home/Home';
+import ProductListing from './pages/Productlisting/ProductListing';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar/> */}
-      <Slider/>
-      <Newsletter/>
+      <Navbar/>
       <Footer/>
     </div>
   );

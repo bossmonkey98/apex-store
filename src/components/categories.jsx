e61db8cd5a8e5cf5fa7e678c586f841b1965d08a
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-03-28 23:10:38 +0530

    minor changes

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -31,7 +31,9 @@ position:absolute;
 top:0;
 bottom:0;
 width: 100%;
-color:red;`;
+color:White;
+background-color:var(--bg-color);
+opacity:0.7;`;
 
 const Button = styled.button`
 cursor: pointer;

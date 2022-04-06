0ea7c5490058132ba68ce6acbf8f4fbc7ba6aae3
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-04-06 14:49:25 +0530

    [feat]: wishlist component added

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useWishlist } from '../../components/context/wishlist-context'
+import {Card} from '../../components/Card/Card'
+import './Wishlist.css'
 
-export default function Wishlist(){
+export  function Wishlist(){
+  const { wishlist } = useWishlist()
+
   return (
-    <div>
-      Wishlist
+    <div className='wishlist-Container' >
+      {wishlist.length ===0? <>
+        <h1>Oops Your List is Empty Browse Products</h1>
+      </>:<><h1>Your WishList Items</h1>
+      <div className='wishlist-content'>
+        {wishlist.map((items)=>(
+          <Card items ={items}/>
+        ))}
+      </div></>}
     </div>
   )
 }

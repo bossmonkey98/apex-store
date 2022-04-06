5c404dd2a6de44341469b2e712b5c1449cd5a913
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-04-06 14:47:10 +0530

    Wishlist Context provider added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { makeServer } from "./server";
 import {BrowserRouter} from 'react-router-dom';
 import {ProductProvider} from './components/context/product-context'
 import { FilterProvider } from "./components/context/filter-context";
+import {WishlistProvider} from "./components/context/wishlist-context"
+import { AuthProvider } from "./components/context/auth-context";
+
 // Call make Server
 makeServer();
 
@@ -13,9 +16,13 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <ProductProvider>
-        <FilterProvider>
-          <App />
-        </FilterProvider>
+        <AuthProvider>
+          <WishlistProvider>
+            <FilterProvider>
+              <App />
+            </FilterProvider>
+          </WishlistProvider>
+        </AuthProvider>  
       </ProductProvider>
     </BrowserRouter>
   </React.StrictMode>,

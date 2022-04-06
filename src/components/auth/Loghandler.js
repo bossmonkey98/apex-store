433d812049a1a56551f130e7241095d2316ef9b5
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-04-06 14:42:43 +0530

    [feat]: login handler func to post user data

---
+++ src/components/auth/Loghandler.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const LogInHandler = ({ email, pass }, setUser, navigator) => {
+  (async () => {
+    console.log(email,pass)
+    try {
+      const { data, status } = await axios.post("/api/auth/login",JSON.stringify({
+        email: email,
+        password: pass,
+      }));
+      if (status === 200) {
+        localStorage.setItem("token", JSON.stringify(data.encodedToken));
+        setUser({tokenVal:JSON.stringify(data.encodedToken), isUserLoggedIn: true });
+        navigator("/");
+      }
+    } catch (error) {
+      console.error("Invalid email or password", error);
+    }
+  })();
+};
+
+export const LogoutHandler = (setUser) => {
+    localStorage.removeItem("token");
+    setUser({ isUserLoggedIn: false });
+  };

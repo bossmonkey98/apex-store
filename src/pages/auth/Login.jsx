117e8df2156e74371019cd006a1c3ea2770d8bad
Arun Kumar <47918140+bossmonkey98@users.noreply.github.com>
2022-06-09 09:41:25 +0530

    Update Login.jsx

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import './auth.css'
 import { Link, useNavigate} from 'react-router-dom'
-import {useAuth} from '../../components/context/auth-context'
-import {LogInHandler} from '../../components/auth/Loghandler'
+import {useAuth} from '../../context/auth-context'
+import {LogInHandler} from '../../services/auth/Loghandler'
 
 
 export default function Login() {
